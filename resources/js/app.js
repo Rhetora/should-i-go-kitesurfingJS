c15e34f385b093ef56b853144728f906f221765c
Aran <aranmcc7@outlook.com>
2024-01-21 20:03:57 +0000

    shows wind speeds and direction, css changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,22 +3,28 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
-const apiKey = "5e1df15d1ca6979db5d1e2028ddf3643";
+const openweathermapKey = "5e1df15d1ca6979db5d1e2028ddf3643";
+
+//const url2 = "https://api.open-meteo.com/v1/forecast?latitude=51.41326&longitude=4.20546&hourly=wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_direction_10m,wind_direction_80m,wind_direction_120m&forecast_days=1&models=ecmwf_ifs04"
 
 
+form.addEventListener("submit", addLocation);
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    let input = document.querySelector(".top-banner input");
+function addLocation(event) {
+    event.preventDefault();
+
+    //Input pprocess
     const inputVal = input.value.toLowerCase().replace(" ", "").replace("uk", "gb");
 
+    //Get tile list
     const listItems = list.querySelectorAll(".ajax-section .city");
     const listItemsArray = Array.from(listItems);
+
+    //handle duplicate location
     if (listItemsArray.length > 0) {
-        console.log(listItemsArray);
         const filteredArray = listItemsArray.filter(el => {
             let content = "";
-            //athens,gr 
+
             if (inputVal.includes(",")) {
                 //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal 
                 if (inputVal.split(",")[1].length > 5) {
@@ -43,25 +49,41 @@ form.addEventListener("submit", e => {
         }
     }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${openweathermapKey}&units=metric`;
+
+    locationMarkup(url);
+}
+
+function locationMarkup(url) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const { main, name, sys, weather } = data;
+            const { main, name, sys, weather, wind } = data;
+            console.log(data);
             const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
             const li = document.createElement("li");
             li.classList.add("city");
-            const markup = ` 
+            const markup = `
+        <div class="city-grid">
         <h2 class="city-name" data-name="${name},${sys.country}"> 
         <span>${name}</span> 
         <sup>${sys.country}</sup> 
         </h2> 
         <div class="city-temp">${Math.round(main.temp)}<sup>&deg;C</sup> 
-        </div> 
+        </div>
         <figure> 
         <img class="city-icon" src=${icon} alt=${weather[0]["main"]}> 
         <figcaption>${weather[0]["description"]}</figcaption> 
-        </figure> 
+        </figure>
+        </div>
+        <div class="city-grid">
+        <div class="city-wind">${wind.speed}<sup>mph</sup> 
+        </div>
+        <div class="city-wind city-gust">${wind.gust}<sup>mph</sup> 
+        </div>
+        <div class="city-arrow" style="transform: rotate(${wind.deg-180}deg);">&uarr;
+        </div>
+        </div>
         `;
             li.innerHTML = markup;
             list.appendChild(li);
@@ -70,8 +92,8 @@ form.addEventListener("submit", e => {
             msg.textContent = "Please search for a valid city!";
         });
 
+
     msg.textContent = "";
     form.reset();
     input.focus();
-});
-
+}
