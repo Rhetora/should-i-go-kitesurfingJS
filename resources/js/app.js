986246e2e235e6d763cf7f429ebd822d9e2ed536
Aran <aranmcc7@outlook.com>
2024-01-24 20:05:41 +0000

    formatting, and weight input field

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector(".top-banner form");
-const input = document.querySelector(".top-banner input");
+const input = document.querySelector(".top-banner .input-city");
+const inputWeight = document.querySelector(".input-weight");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
@@ -7,8 +8,9 @@ const openweathermapKey = "5e1df15d1ca6979db5d1e2028ddf3643";
 
 //const url2 = "https://api.open-meteo.com/v1/forecast?latitude=51.41326&longitude=4.20546&hourly=wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_direction_10m,wind_direction_80m,wind_direction_120m&forecast_days=1&models=ecmwf_ifs04"
 
-
 form.addEventListener("submit", addLocation);
+
+inputWeight.addEventListener("change", updateKiteSize);
 
 list.addEventListener("click", removeLocation);
 
@@ -16,7 +18,10 @@ function addLocation(event) {
     event.preventDefault();
 
     //Input pprocess
-    const inputVal = input.value.toLowerCase().replace(" ", "").replace("uk", "gb");
+    const inputVal = input.value
+        .toLowerCase()
+        .replace(" ", "")
+        .replace("uk", "gb");
 
     //Get tile list
     const listItems = list.querySelectorAll(".ajax-section .city");
@@ -24,24 +29,26 @@ function addLocation(event) {
 
     //handle duplicate location
     if (listItemsArray.length > 0) {
-        const filteredArray = listItemsArray.filter(el => {
+        const filteredArray = listItemsArray.filter((el) => {
             let content = "";
 
             if (inputVal.includes(",")) {
-                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal 
+                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
                 if (inputVal.split(",")[1].length > 5) {
                     inputVal = inputVal.split(",")[0];
-                    content = el.querySelector(".city-name span").textContent.toLowerCase();
+                    content = el
+                        .querySelector(".city-name span")
+                        .textContent.toLowerCase();
                 } else {
                     content = el.querySelector(".city-name").dataset.name.toLowerCase();
                 }
             } else {
-                //athens 
+                //athens
                 content = el.querySelector(".city-name span").textContent.toLowerCase();
             }
             return content == inputVal.toLowerCase();
         });
-        //3 
+        //3
         if (filteredArray.length > 0) {
             msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city-name span").textContent
                 } ...otherwise be more specific by providing the country code as well`;
@@ -60,13 +67,12 @@ function removeLocation(event) {
     if (event.target.classList.contains("close-button")) {
         event.target.parentElement.parentElement.remove();
     }
-
 }
 
 function locationMarkup(url) {
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             const { main, name, sys, weather, wind } = data;
             if (wind.gust == null) wind.gust = wind.speed;
             const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
@@ -83,19 +89,27 @@ function locationMarkup(url) {
                     <span>${name}</span> 
                     <sup>${sys.country}</sup> 
                 </h2> 
-                <div class="city-temp">${Math.round(main.temp)}<sup>&deg;C</sup> 
+                <div class="city-temp">${Math.round(
+                main.temp
+            )}<sup>&deg;C</sup> 
                 </div>
                 <figure> 
-                    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}> 
+                    <img class="city-icon" src=${icon} alt=${weather[0]["main"]
+                }> 
                     <figcaption>${weather[0]["description"]}</figcaption> 
                 </figure>
             </div>
             <div class="city-grid">
-                <div class="city-wind">${(wind.speed * 0.868976).toFixed(2)}<sup>kts</sup> 
+                <div class="city-wind">${(wind.speed * 0.868976).toFixed(
+                    2
+                )}<sup>kts</sup> 
                 </div>
-                <div class="city-wind city-gust">${(wind.gust * 0.868976).toFixed(2)}<sup>kts</sup> 
+                <div class="city-wind city-gust">${(
+                    wind.gust * 0.868976
+                ).toFixed(2)}<sup>kts</sup> 
                 </div>
-                <div class="city-arrow" style="transform: rotate(${wind.deg - 180}deg);">&uarr;
+                <div class="city-arrow" style="transform: rotate(${wind.deg - 180
+                }deg);">&uarr;
                 </div>
             </div>
         </div>
@@ -110,4 +124,15 @@ function locationMarkup(url) {
     msg.textContent = "";
     form.reset();
     input.focus();
+}
+
+function updateKiteSize(event) {
+    let weight = parseInt(inputWeight.value);
+    if (!Number.isInteger(weight)) {
+        inputWeight.value = "";
+        inputWeight.placeholder = "Enter a number!";
+        return;
+    }
+    inputWeight.value = weight+"kgs";
+    console.log(weight);
 }
